eb83603d5aa4cbb69acb6e599e79564d44e75153
KrecikKretes <79220080+KrecikKretes@users.noreply.github.com>
2022-07-06 23:17:25 +0200

    Add files via upload

---
+++ beep.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main () {
+	
+	Beep(65.4, 800);
+	Beep(0, 200);
+	Beep(65.4, 200);
+	Beep(65.4, 200);
+	Beep(65.4, 400);
+	Beep(87.3, 400);
+
+	
+	
+	
+	
+}
